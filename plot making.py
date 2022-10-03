e491948f095fa0b31fe4966e029a627550f398b2
S M Shaikhul Islam <112336036+smsitayef@users.noreply.github.com>
2022-10-03 16:20:16 +0600

    Add files via upload

---
+++ plot making.py
@@ -0,0 +1,26 @@
+
+
+import os
+import pandas as pd
+from matplotlib import pyplot as plt
+
+
+#set working directory and read data
+
+path = '/Users/user/Desktop'
+os.chdir(path)
+a = pd.read_csv('Ogallaladata.csv')
+
+# extract WD and DWT
+
+WD = a.WellDepth
+DWT = a.DWT
+
+plt.plot(WD, DWT,'ro') #ro for red
+
+plt.xlabel('Well Depth, ft')
+plt.ylabel('Depth of Water table(ft)')
+plt.grid() #a grid for aesthetics
+plt.title('Wall Depth to ater table relationship')
+plt.legend(['DWT'],loc='upper left')
+plt.show()
